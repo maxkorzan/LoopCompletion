3a82969863661a5944e3545dd5889c21493adecf
maxkorzan <maxkorzan@gmail.com>
2020-01-23 10:20:15 -0500

    first commit

---
+++ src/GetIntegers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GetIntegers {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int num, total = 0;
+
+        // Loop should start here
+        for(int i=0; i<10; i++){
+            System.out.print("Enter a number: ");
+            num = keyboard.nextInt();
+            total += num;
+        }
+        // Loop should end here
+
+        System.out.println("The total of all 10 numbers is " + total);
+
+    }
+
+}
